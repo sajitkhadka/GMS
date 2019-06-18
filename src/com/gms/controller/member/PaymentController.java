85c2a586953a90b9188d8637467cde7fac8f0b5b
Sajit Khadka <sajitkhadka@gmail.com>
2019-06-18 08:10:09 -0400

    changes by sajit

---
+++ src/com/gms/controller/member/PaymentController.java
@@ -0,0 +1,15 @@
+package com.gms.controller.member;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PaymentController{
+
+}
